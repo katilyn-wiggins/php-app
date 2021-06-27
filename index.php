e50ffeb3fe6fa232c7e3217b730f2caa81909fd7
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-06-27 15:33:42 -0400

    home and search page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome To Legoog</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+  test font here 
+    <div class="mainSection">
+
+      <div class="logoContainer">
+        <img src="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png" alt="google" height="150px">
+      </div>
+
+      <div class="searchContainer">
+
+        <form action="search.php" method="GET">
+
+          <input type="text" class="searchBox" name="term" >
+          <input type="submit" class="searchButton" value="Search">
+        
+        </form>
+
+      </div>
+
+    </div>
+
+</body>
+</html>
