8265741243e5404b16dafae4d996e87021dd846b
Katilyn Wiggins <32716761+katilyn-wiggins@users.noreply.github.com>
2021-07-08 10:21:58 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome To Legoog</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+  <div class="wrapper indexPage"> 
+
+      <div class="mainSection">
+
+        <div class="logoContainer">
+          <img src="assets/images/legoog.png" alt="google" height="150px">
+        </div>
+
+        <div class="searchContainer">
+
+          <form action="search.php" method="GET">
+
+            <input type="text" class="searchBox" name="term" >
+            <input type="submit" class="searchButton" value="Search">
+          
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
+</body>
+</html>
